3117cd305c6b62233b3a2f85e5c95ebdc1638db3
indhoh27 <yaanbo306@gmail.com>
2026-03-13 16:47:36 +0300

    new update

--- src/pages/ProgramDetail.tsx
+++ src/pages/ProgramDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { motion } from "framer-motion";
 import { ChevronRight, Loader2 } from "lucide-react";
